b63c303509b4590e8e091a7b6e3e147cafb9e27e
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-17 16:46:33 -0500

    Create Location class (location.dart) and move try/catch code as a class method. Create a Location object on loading screen and call this class method in initState().

---
+++ lib/services/location.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude = 0.0;
+  double longitude = 0.0;
+
+  // Location(this.latitude, this.longitude);
+
+  Future<void> getCurrentLocation() async {
+    try {
+      // Request Permission for Position and Get.
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = (await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low));
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
