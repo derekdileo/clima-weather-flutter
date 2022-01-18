f4db3fd5ad5d985f971509ac06ced027d80f020e
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-18 10:15:33 -0500

    Refactor loading screen by removing latitude and logitude variables and inserting directly into Uri parse string.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima_weather_flutter/services/location.dart';
 import 'package:clima_weather_flutter/services/networking.dart';
+import 'package:clima_weather_flutter/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const String apiKey = '1d931b610ea125c7e78c075d7bfbe4a5';
 const String weatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
@@ -11,9 +13,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  double latitude = 0.0;
-  double longitude = 0.0;
-
   @override
   void initState() {
     super.initState();
@@ -21,21 +20,39 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   Future<void> getLocationData() async {
+    // Instantiate Location object
     Location location = Location();
+
+    // Gather lat / long via getCurrentLocation()
     await location.getCurrentLocation();
-    latitude = location.latitude;
-    longitude = location.longitude;
 
-    var url =
-        Uri.parse('$weatherMapURL?lat=$latitude&lon=$longitude&appid=$apiKey');
+    // Generate a Uri for API call
+    var url = Uri.parse(
+        '$weatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=imperial');
 
+    // Instantiate NetworkHelper object with Uri
     NetworkHelper networkHelper = NetworkHelper(url);
 
+    // Gather jsonDecoded weatherData
     var weatherData = await networkHelper.getData();
+
+    // Build Location Screen, pass weatherData to its constructor.
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
